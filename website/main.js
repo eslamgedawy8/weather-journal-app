0bc9d57fe08d5712e5ed477ec7f53c5f06ff3fef
eslam elgedawy <e.elgedawy@brimore.com>
2022-02-11 16:15:11 +0200

    rewrite main.js

---
+++ website/main.js
@@ -0,0 +1,80 @@
+/* Global Variables */
+
+// Create a new date instance dynamically with JS
+let d = new Date()
+let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
+let newDate = new Intl.DateTimeFormat('en-US', options).format(d)
+
+const url = "https://api.openweathermap.org/data/2.5/weather?zip="
+const apiKey = "c4fbf63796ec8738faf7bea59de8f66a"
+const server = "http://127.0.0.1:4000"
+
+document.getElementById('generate').addEventListener('click', function () {
+    let zipCode = document.getElementById('zip').value
+    let feelings = document.getElementById('feelings').value
+
+    if(!zipCode) {
+        document.getElementById('error').innerText = 'Please enter your zip code!!'
+    }else{
+        // get weather data 
+        getData(zipCode).then(val => {
+            let temp = Math.round(val.main.temp)
+            let description = val.weather[0].description
+            let city = val.name
+            const objVal = {
+                temp,
+                description,
+                city,
+                feelings,
+                newDate
+            }
+            console.log('objVal: ', objVal)
+            // post data
+            postData(`${server}/add-weather`, objVal)
+            // render UI
+            updateUI()
+        })
+    }
+})
+
+
+
+const getData =  async (zipCode) => {
+  const res = await fetch(`${url}${zipCode},us&appid=${apiKey}`)
+  try {
+      const data = await res.json()
+      return data 
+  } catch (error) {
+      console.log('error: ', error)
+  }
+}
+
+const postData = async (url, data = {}) => {
+    const response = await fetch(url, {
+        method: 'POST', 
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data) 
+    })
+    try {
+        const data = await response.json()
+        return data
+    } catch (error) {
+        console.log('error: ', error)
+    }
+}
+
+const updateUI = async () => {
+   const res = await fetch(`${server}/get-weather`)
+   try {
+       const data = await res.json()
+       document.getElementById('date').innerText = data.newDate
+       document.getElementById('temp').innerText = data.temp
+       document.getElementById('description').innerText = data.description
+       document.getElementById('content').innerText = data.feelings
+       document.getElementById('city').innerText = data.city
+   } catch (error) {
+       console.log('error: ', error)
+   }
+}
